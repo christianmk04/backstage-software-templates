e1f61580603a8df60cdd87d0ec873a57e96ec758
Christian Koh <ckoh.2021@scis.smu.edu.sg>
2025-11-11 10:50:22 +0800

    Adds python app software template

---
+++ python-app/template/src/app.py
@@ -0,0 +1,28 @@
+from flask import Flask, jsonify
+import datetime
+import socket
+
+app = Flask(__name__)
+
+@app.route('/api/v1/info')
+def info():
+    return jsonify({
+        'hostname': socket.gethostname(),
+        'time': datetime.datetime.now().strftime("%I:%M:%S%p on %B %d, %Y"),
+        "message": "Details endpoint is working! :)",
+        'deployed_on': 'kubernetes',
+        'env': '${{values.app_env}}',
+        'app_name' : '${{values.app_name}}',
+    }), 200
+
+@app.route('/api/v1/health')
+def health():
+    return jsonify({
+        'message': 'Health endpoint is working!',
+        'status': 'healthy'
+    }), 200
+
+
+if __name__ == '__main__':
+    # We need '0.0.0.0' to be accessible from outside the container
+    app.run(debug=True, host='0.0.0.0')
